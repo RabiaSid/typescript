228e833d93f1f17beb7ecf374be760038a3e9e92
RabiaSid <rabiasid984@gmail.com>
2023-05-29 21:37:17 +0500

    generator

--- app.js
+++ app.js
@@ -1,67 +1,46 @@
 // let num1  = 1;
-
 // function calculate(num1: number, num2: number){
 //     return num1 + num2
 // }
-
 // console.log(calculate(num1,2))
-
 // function (numbers: Array<number>) {
-    
 //     return numbers.reduce((acc , item) => {
-    
 //         return acc + item;
-   
 //     }, 0);
-
 // }
-
 // console.log(Total([3,2,1]));
 // const User = {
 //     firstName : 'john',
 //     lastname:'Doe',
 //     role: 'professor'
 // }
-
-
 // console.log(User.firstName)
-
 //Type arias
 // type User = {
 //     name: string,
 //     age: number,
 //     address?: string
 // }
-
-
-
 // if we add 'void' that mean we do not have any return in func
 // function login(userData: User): void{
 //     return 
 // }))
-
 // const user : User = {
 //     name:'Rabia',
 //     age:23,
 //     // address: 'njsackjsh'
 // }
-
 // function login(userData: User): User{
 //     return userData;
 // }
 // console.log(login(user))
-
 // type ID = number | string;
-
 // const UserId: ID = 123;
-
 //InterFaces 
-
 // interface Trasaction {
 //     payerAccountNumber: number;
 //     payeeAccountNumber: number;
 // }
-
 // interface BankAccount {
 //     accountNUmber : number;
 //     accountHolder: string;
@@ -69,7 +48,6 @@
 //     inActive: boolean;
 //     transaction: Trasaction[]
 // } 
-
 // const transaction1: Trasaction ={
 //     payerAccountNumber: 123,
 //     payeeAccountNumber: 321
@@ -85,76 +63,39 @@
 //     inActive: true,
 //     transaction: [transaction1,transaction2]
 // }
-
-
 // interface Book{
 //     name: string;
 //     price: number;
 // }
-
 // interface Ebook extends Book{
 //     fileSize: number;
 //     format: string;
 // }
-
 // interface AudioBook extends Ebook{
 //     duration: number;
 // }
-
 // const book: AudioBook = {
 //     name:'atomic habits',
 //     price: 25 ,
 //     fileSize: 2,
 //     format:'mnkj',
 //     duration:22
-
-
 // }
-
 // interface  Book {
 //     name: string;
 //     price: number;
 // }
-
 // interface  Book {
 //     size: number;
 // }
-
 // const book : Book ={
 //     name:'atomic habits',
 //     price: 25 ,
 //     size: 2,
 // }
-
 //union
 //   
-
-// function getFirstThree(x: string | number[]){
-//     return x.slice(0.3);
-// }
-
-// console.log(getFirstThree([1, 2, 3, 4, 5]))
- 
-//Generics
-// 
-function logString(arg: string) {
-    console.log(arg);
-    return arg;
+function getFirstThree(x) {
+    return x.slice(0.3);
 }
-
-function logNumber(arg: number) {
-    console.log(arg);
-    return arg;
-}
-
-function logArray(arg: any[]) {
-    console.log(arg);
-    return arg;
-}
-
-function logAnything<T>(arg: T): T {
-    console.log(arg);
-    return arg;
-}
-
-logAnything(['hello']);
+console.log(getFirstThree([1, 2, 3, 4, 5]));
